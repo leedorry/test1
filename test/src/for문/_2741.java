160bc1c24d8e433141d03c5e022a5e4651f6c02a
leedorry <iklesd@gmail.com>
2021-09-05 22:07:42 +0900

    알고리즘 (2021.09.05)

---
+++ test/src/for문/_2741.java
@@ -0,0 +1,20 @@
+package for문;
+
+import java.util.Scanner;
+
+public class _2741 {
+
+	public static void main(String[] args) {
+
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for(int i=1; i<=n; i++) {
+			System.out.println(i);
+		}
+	
+	}
+
+}
---
+++ test/src/for문/_2742.java
@@ -0,0 +1,19 @@
+package for문;
+
+import java.util.Scanner;
+
+public class _2742 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=n; i>0; i--) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
